8f85ad00f3407437f86913e4a576f3c62f320e1c
ckoenig <ckoenig@9c6bb43a-1051-0410-9f52-8985cf0311e8>
2010-02-08 22:43:23 +0000

    Fixing up gitweb configuration, and adding a gitweb dotfile. Also modifying columbo_syntax_check.sh to use the correct perl syntax check flag, and clean up its output some.
    git-svn-id: svn+ssh://svn.corp.yahoo.com/yahoo/users/ckoenig@6399 9c6bb43a-1051-0410-9f52-8985cf0311e8

--- hudson/columbo_syntax_check.sh
+++ hudson/columbo_syntax_check.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
 
-ANY_ERRORS=0
+SRC_DIRECTORY="/home/y/lib/perl5/site_perl/Yahoo/"
+if [[ "$1" = "src" ]]; then
+    SRC_DIRECTORY="/home/ckoenig/src/columbo/src/perl/Yahoo/"
+fi
 
-for FILE in `find /home/y/lib/perl5/site_perl/Yahoo/ -type f -name "*.pm"`; do
-    echo -n "Checking syntax of $FILE..."
-    perl -C $FILE
-    if [[ $? -eq 0 ]]; then
-        echo " [ OK ]"
-    else
+ANY_ERRORS=0
+for FILE in `find $SRC_DIRECTORY -type f -name "*.pm"`; do
+    perl -Xc $FILE
+    if [[ $? -ne 0 ]]; then
+        WRONG_FILES="$WRONG_FILES $FILE"
         ANY_ERRORS=1
     fi
 done
+
+if [[ "$ANY_ERRORS" = "1" ]]; then
+    echo
+    echo "Syntax check failed:"
+    for FILE in $WRONG_FILES; do
+        echo "  $FILE"
+    done
+fi
 
 exit $ANY_ERRORS
